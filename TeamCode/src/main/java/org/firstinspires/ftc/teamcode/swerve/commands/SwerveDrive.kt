733ee2484df98f4a25fe62124533db80d7db5259
Peter-Ryden1 <140096651+Peter-Ryden1@users.noreply.github.com>
2023-11-27 20:43:21 -0500

    moved over swerve drive.  Added ftclib, ftcdashboard

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/commands/SwerveDrive.kt
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.swerve.commands
+
+import com.arcrobotics.ftclib.command.CommandBase
+import com.arcrobotics.ftclib.geometry.Pose2d
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.ChassisSpeeds
+import org.firstinspires.ftc.teamcode.swerve.subsystems.SwerveDriveBase
+
+class SwerveDrive(private val leapfrog: SwerveDriveBase,
+                  private val initialPose : Pose2d,
+                  private val chassisSpeedsProvider : () -> ChassisSpeeds) : CommandBase() {
+    init {
+        addRequirements(leapfrog)
+    }
+    override fun initialize() {
+        leapfrog.initialize(initialPose)
+    }
+    override fun execute() {
+        leapfrog.drive(chassisSpeedsProvider())
+    }
+}
