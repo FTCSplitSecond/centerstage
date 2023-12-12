f4eac7219d7192a59d2d4f94cda72300947cbb71
Pete Ryden <ryden25p@ncssm.edu>
2023-12-12 17:40:19 -0500

    Made an april tag subsystem class and set up concept april tag test opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/subsystems/AprilTags.kt
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.vision.subsystems
+
+import com.arcrobotics.ftclib.command.Subsystem
+import com.arcrobotics.ftclib.command.SubsystemBase
+
+class AprilTags : SubsystemBase() {
+
+
+}
