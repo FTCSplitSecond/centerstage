47f38cda0649b24710d50cc2a5690e2b57e0c6c5
Arjun Rao <rao.rjun@gmail.com>
2023-12-29 22:09:17 -0500

    started an opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/opmodes/PropDetectorTest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.vision.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+
+public class PropDetectorTest extends LinearOpMode {
+
+    OpenCvCamera webcam;
+
+    //private VisionPortal visionPortal;
+    @Override
+    public void runOpMode() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+        
+
+    }
+}
