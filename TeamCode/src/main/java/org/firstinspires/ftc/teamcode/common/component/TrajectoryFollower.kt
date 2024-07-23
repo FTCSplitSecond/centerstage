105c074d4e2277911b6088eb2e6142a4237409c4
polargh <io@itspolar.dev>
2024-07-23 04:48:49 -0500

    tele should drive?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/component/TrajectoryFollower.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/component/TrajectoryFollower.kt
@@ -1,4 +1,20 @@
 package org.firstinspires.ftc.teamcode.common.component
 
-class TrajectoryFollower {
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.acmerobotics.roadrunner.trajectory.Trajectory
+import dev.turtles.anchor.component.FinishReason
+import org.firstinspires.ftc.teamcode.roadrunner.drive.CenterstageMecanumDrive
+
+class TrajectoryFollower(val drive: CenterstageMecanumDrive, val trajectory: Trajectory): SplitSecondComponent() {
+    override fun start() {
+        drive.followTrajectoryAsync(trajectory)
+    }
+
+    override fun isComplete(): Boolean = !drive.isBusy
+
+    override fun end(reason: FinishReason) {
+        if (reason != FinishReason.COMPLETED) {
+            drive.setWeightedDrivePower(Pose2d(0.0, 0.0, 0.0))
+        }
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.kt
@@ -2,7 +2,10 @@ package org.firstinspires.ftc.teamcode.opmodes
 
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import dev.turtles.anchor.component.stock.delay
 import dev.turtles.anchor.component.stock.idler
+import dev.turtles.anchor.component.stock.instant
+import dev.turtles.anchor.component.stock.series
 import dev.turtles.electriceel.opmode.DSLOpMode
 import dev.turtles.electriceel.util.Pose
 import dev.turtles.electriceel.util.Vector2
@@ -15,7 +18,15 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference
 import org.firstinspires.ftc.teamcode.OffseasonBot
+import org.firstinspires.ftc.teamcode.common.component.DriveMecanum
 import org.firstinspires.ftc.teamcode.common.types.OpModeType
+import org.firstinspires.ftc.teamcode.roadrunner.drive.DriveConstants
+import org.firstinspires.ftc.teamcode.subsystem.ClawSubsystem
+import org.firstinspires.ftc.teamcode.subsystem.DepositSubsystem
+import org.firstinspires.ftc.teamcode.subsystem.DroneSubsystem
+import kotlin.math.absoluteValue
+import kotlin.math.pow
+import kotlin.math.sign
 
 @TeleOp(name = "Primary TeleOp", group = "!")
 class Teleop: DSLOpMode(false, {
@@ -55,8 +66,51 @@ class Teleop: DSLOpMode(false, {
     val driverRightTrigger = EventTrigger { driver[Button.Trigger.RIGHT] > triggerThresh }
     val driverLeftTrigger = EventTrigger { driver[Button.Trigger.LEFT] > triggerThresh }
 
+    driver[Button.Key.TRIANGLE] onActivate deposit.setArmState(DepositSubsystem.State.STACK_INTAKE_CLOSED)
+
+    val shapeJoystickResponse: (Double) -> Double = {
+        val shapedPower = it.absoluteValue.pow(DriveConstants.JOYSTICK_EXPONENT) * sign(it)
+        shapedPower
+    }
+
+    val command = DriveMecanum(
+        robot.drivetrain,
+        {
+            val leftY = -driver[Button.Joystick.LEFT].y
+            shapeJoystickResponse(leftY)
+        },
+        {
+            val leftX = driver[Button.Joystick.LEFT].x
+            shapeJoystickResponse(leftX)
+        },
+        {
+            val rightX = driver[Button.Joystick.RIGHT].y * DriveConstants.OMEGA_WEIGHT
+            if (deposit.armState == DepositSubsystem.State.EXTENDED_INTAKE || deposit.armState == DepositSubsystem.State.DEPOSIT) {
+                rightX * 0.5
+            }
+
+            shapeJoystickResponse(rightX)
+        }
+    )
+
+    schedule(command)
+
     val routine = RoutineModule { true }
     driver.apply(routine)
 
+    driver[Button.Key.DPAD_DOWN] onActivate instant {
+        robot.driverStationOffset = robot.drivetrain.poseEstimate().heading
+    }
 
+    driver[Button.Key.SHARE] onActivate instant {
+        drone.pitchPosition = DroneSubsystem.PitchPositions.LAUNCHED
+    }
+
+    driver[Button.Key.SHARE] onDeactivate series(
+        instant {
+            drone.launched = true
+        },
+        delay(0.25),
+        instant { drone.pitchPosition = DroneSubsystem.PitchPositions.STOWED }
+    )
 })
